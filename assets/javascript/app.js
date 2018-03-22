6f0e15799995c3be1144277e20505016a3ceca45
Ramzi Dreessen <ramzidreessen@gmail.com>
2018-03-22 18:53:41 -0500

    added timer, added design

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 
-    // global variables
+    // hide some things initially
+    $(".js-results").hide();
+    $(".js-stop").hide();
 
+    // global variables
     let questions = [
         {
             "question": "What is Marges maiden name?",
@@ -17,16 +20,54 @@ $(document).ready(function() {
             "question": "What instrument does Lisa play?",
             "answers": ["Violin", "Sax", "Oboe", "Cowbell"],
             "correctAnswer": "Sax",
+        },
+        {
+            "question": "What's the name of the town the show takes place in?",
+            "answers": ["Springfield", "Chicago", "Hartford", "South Bend"],
+            "correctAnswer": "Springfield",
         }
     ]
     
     let userAnswer = null;
     let userQuestion = null;
+    let gameReady = true;
+    let gameStopped = false;
+
+    let count = 30;
+    let counter = null;
+
+    let userCorrect = 0;
+    let userInCorrect = 0;
     
 
     // functions
 
+    function timer() {
+        count--;
+
+        if (count <= 0) {
+            if (gameStopped !== true) {
+                alert("Time's up!");
+            }
+            clearInterval(counter);
+            stopGame();
+        } else {
+            $(".js-timer").text(count + " seconds left!");
+        }
+    }
+
     function startGame () {
+
+        let counter = setInterval(timer, 1000);
+
+        // 30-second timer
+        $(".js-timer").show().text(count + " seconds left!");
+
+        // show the stop button, hide the start button
+        $(".js-stop").show();
+        $(".js-start").hide();
+
+        // show questions
         for (let i = 0; i < questions.length; i++) {
             $('.js-questions').append('<p>' + questions[i].question + '</p>');
 
@@ -36,45 +77,58 @@ $(document).ready(function() {
 
             $('.js-questions').append('<br>');
         }
+
+        // uncheck the checkboxes
+        $('.js-questions input').removeAttr("checked");
+
+        // while playing, game is not ready to start
+        gameReady = false;
+        
     }
 
     function stopGame () {
+        
+        gameStopped = true;
+        clearInterval(counter);
+        $(".js-timer").hide();
+
         $('.js-questions input:checked').each(function() {
             userAnswer = $(this).val();
             userQuestion = $(this).attr('name');
             
             if (userAnswer === questions[userQuestion].correctAnswer) {
-                console.log("you are correct!");
+                userCorrect++;
             } else {
-                console.log("D'oh!");
+                userInCorrect++;
             }
         });
+
+        $(".js-results").fadeIn();
+        $("#correct").text(userCorrect);
+        $("#incorrect").text(userInCorrect);
+
+        userCorrect = 0;
+        userInCorrect = 0;
     }
+
 
     // events
     // click start button, will start the game
     $('.js-start').on('click', function () {
-        startGame();
+        if (gameReady) {
+            startGame();
+        }
     });
 
     // click stop button to see score
     $('.js-stop').on('click', function () {
         stopGame();
+        gameReady = true;
     });
 
-    // timer runs out to end game and show score
-    var count=30;
-
-    var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
-
-    function timer() {
-        count=count-1;
-        if (count <= 0) {
-            clearInterval(counter);
-            return;
-        }
-
-        $(".js-timer").text(count + " secs");
-    }
+    // click this button to reload and try again
+    $('.js-reload').on('click', function () {
+        location.reload();
+    });
 
 });
